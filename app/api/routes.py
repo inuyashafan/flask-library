ba0d26745ba11adad29d13ef46409cacdd212f20
John Abreu <johnabreu67@gmail.com>
2023-10-17 20:35:27 -0500

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Book, book_schema, books_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'yee': 'haw'}
+
+@api.route('/books', methods = ['POST'])
+@token_required
+def create_contact(current_user_token):
+    ISBN = request.json['ISBN']
+    author = request.json['author']
+    book_title = request.json['book_title']
+    book_length = request.json['book_length']
+    book_type = request.json['book_type']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    book = Book(ISBN, author, book_title, book_length, book_type, user_token = user_token )
+
+    db.session.add(book)
+    db.session.commit()
+
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books', methods = ['GET'])
+@token_required
+def get_book(current_user_token):
+    a_user = current_user_token.token
+    books = Book.query.filter_by(user_token = a_user).all()
+    response = books_schema.dump(books)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['GET'])
+@token_required
+def get_single_contact(current_user_token, id):
+    book = Book.query.get(id)
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['POST','PUT'])
+@token_required
+def update_book(current_user_token,id):
+    book = Book.query.get(id) 
+    book.ISBN = request.json['ISBN']
+    book.author = request.json['author']
+    book.book_title = request.json['book_title']
+    book.book_length = request.json['book_length']
+    book.book_type = request.json['book_type']
+    book.user_token = current_user_token.token
+
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['DELETE'])
+@token_required
+def delete_book(current_user_token, id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
